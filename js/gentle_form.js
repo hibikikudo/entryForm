f8601b0fc78baa542545743939ae355c510b8365
hibiki kudo <h.kudo@tribe.bz>
2015-12-07 11:16:19 +0900

    gather gentleform.js

--- js/gentle_form.js
+++ js/gentle_form.js
@@ -596,3 +596,45 @@
     ])
 }
 );
+
+// This is a demo of GentleForm:
+// https://github.com/Zhouzi/GentleForm
+
+var toaster = document.querySelector('.toaster');
+
+new GentleForm('form', function (event, isValid, data) {
+  event.preventDefault();
+  
+  if (isValid) window.location.href = "./confirm.html";
+  else toast('入力エラーがあります。<br>・必須項目の抜けはありませんか?<br>・プライバシーポリシーのチェックが外れていませんか?', 'error');
+  
+  console.log(event, isValid, data);
+});
+
+var timeout;
+function toast (msg, type) {
+  if (type == 'success') {
+    toaster.classList.remove('toaster--error');
+    toaster.classList.add('toaster--success');
+  } else {
+    toaster.classList.remove('toaster--success');
+    toaster.classList.add('toaster--error');
+  }
+  
+  toaster.innerHTML = msg;
+  toaster.style.display = 'block';
+  
+  clearTimeout(timeout);
+  
+  timeout = setTimeout(function () {
+    toaster.classList.add('toaster--show');
+    
+    timeout = setTimeout(function () {
+      toaster.classList.remove('toaster--show');
+      
+      timeout = setTimeout(function () {
+        toaster.style.display = 'none';
+      }, 300);
+    }, 5000);
+  }, 7);
+}
