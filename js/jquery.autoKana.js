8f85d71d12576388ba28ceb86681fad9463c0a57
hibiki kudo <h.kudo@tribe.bz>
2015-12-08 12:12:01 +0900

    Revert "minify js"
    This reverts commit 81b8c67fc03d564b480ef10b44767337bad76e37.

--- js/jquery.autoKana.js
+++ js/jquery.autoKana.js
@@ -21,4 +21,173 @@
 // LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
 // OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 // WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-(function(a){a.fn.autoKana=function(C,B,m){var e=a.extend({katakana:false},m);var t=new RegExp("[^ 　ぁあ-んー]","g");var s=new RegExp("[ぁぃぅぇぉっゃゅょ]","g");var h,f,k=false,q=null,A=true,r;h=a(C);f=a(B);k=true;z();h.blur(l);h.focus(o);h.keydown(j);function i(){k=true}function w(){k=false}function v(G){var F=G||window.event;if(G){var E=Event.element(G);if(E.checked){k=true}else{k=false}}else{k=!k}}function x(E){if(!A){if(Math.abs(values.length-E.length)>1){var F=E.join("").replace(s,"").split("");if(Math.abs(values.length-F.length)>1){u()}}else{if(values.length==r.length&&values.join("")!=r){u()}}}}function p(){var F,E;F=h.val();if(F==""){z();c()}else{F=g(F);if(r==F){return}else{r=F;if(!A){E=F.replace(t,"").split("");x(E);c(E)}}}}function d(){clearInterval(q)}function l(E){d()}function o(E){y();n()}function j(E){if(A){y()}}function b(E){return((E>=12353&&E<=12435)||E==12445||E==12446)}function g(H){if(H.match(ignoreString)){return H.replace(ignoreString,"")}else{var F,G,E;G=ignoreString.split("");E=H.split("");for(F=0;F<G.length;F++){if(G[F]==E[F]){E[F]=""}}return E.join("")}}function n(){var E=this;q=setInterval(p,30)}function c(E){if(!A){if(E){values=E}if(k){var F=D(baseKana+values.join(""));f.val(F)}}}function z(){baseKana="";A=false;ignoreString="";r="";values=[]}function y(){baseKana=f.val();A=false;ignoreString=h.val()}function u(){baseKana=baseKana+values.join("");A=true;values=[]}function D(G){if(e.katakana){var H,E,F;F=new String;for(E=0;E<G.length;E++){H=G.charCodeAt(E);if(b(H)){F+=String.fromCharCode(H+96)}else{F+=G.charAt(E)}}return F}else{return G}}}})(jQuery);
+
+(function ($) {
+    $.fn.autoKana = function (element1, element2, passedOptions) {
+
+        var options = $.extend(
+            {
+                'katakana': false
+            }, passedOptions);
+
+        var kana_extraction_pattern = new RegExp('[^ 　ぁあ-んー]', 'g');
+        var kana_compacting_pattern = new RegExp('[ぁぃぅぇぉっゃゅょ]', 'g');
+        var elName,
+            elKana,
+            active = false,
+            timer = null,
+            flagConvert = true,
+            input;
+
+        elName = $(element1);
+        elKana = $(element2);
+        active = true;
+        _stateClear();
+
+        elName.blur(_eventBlur);
+        elName.focus(_eventFocus);
+        elName.keydown(_eventKeyDown);
+
+        function start() {
+            active = true;
+        };
+
+        function stop() {
+            active = false;
+        };
+
+        function toggle(event) {
+            var ev = event || window.event;
+            if (event) {
+                var el = Event.element(event);
+                if (el.checked) {
+                    active = true;
+                } else {
+                    active = false;
+                }
+            } else {
+                active = !active;
+            }
+        };
+
+        function _checkConvert(new_values) {
+            if (!flagConvert) {
+                if (Math.abs(values.length - new_values.length) > 1) {
+                    var tmp_values = new_values.join('').replace(kana_compacting_pattern, '').split('');
+                    if (Math.abs(values.length - tmp_values.length) > 1) {
+                        _stateConvert();
+                    }
+                } else {
+                    if (values.length == input.length && values.join('') != input) {
+                        _stateConvert();
+                    }
+                }
+            }
+        };
+
+        function _checkValue() {
+            var new_input, new_values;
+            new_input = elName.val()
+            if (new_input == '') {
+                _stateClear();
+                _setKana();
+            } else {
+                new_input = _removeString(new_input);
+                if (input == new_input) {
+                    return;
+                } else {
+                    input = new_input;
+                    if (!flagConvert) {
+                        new_values = new_input.replace(kana_extraction_pattern, '').split('');
+                        _checkConvert(new_values);
+                        _setKana(new_values);
+                    }
+                }
+            }
+        };
+
+        function _clearInterval() {
+            clearInterval(timer);
+        };
+
+        function _eventBlur(event) {
+            _clearInterval();
+        };
+        function _eventFocus(event) {
+            _stateInput();
+            _setInterval();
+        };
+        function _eventKeyDown(event) {
+            if (flagConvert) {
+                _stateInput();
+            }
+        };
+        function _isHiragana(chara) {
+            return ((chara >= 12353 && chara <= 12435) || chara == 12445 || chara == 12446);
+        };
+        function _removeString(new_input) {
+            if (new_input.match(ignoreString)) {
+                return new_input.replace(ignoreString, '');
+            } else {
+                var i, ignoreArray, inputArray;
+                ignoreArray = ignoreString.split('');
+                inputArray = new_input.split('');
+                for (i = 0; i < ignoreArray.length; i++) {
+                    if (ignoreArray[i] == inputArray[i]) {
+                        inputArray[i] = '';
+                    }
+                }
+                return inputArray.join('');
+            }
+        };
+        function _setInterval() {
+            var self = this;
+            timer = setInterval(_checkValue, 30);
+        };
+        function _setKana(new_values) {
+            if (!flagConvert) {
+                if (new_values) {
+                    values = new_values;
+                }
+                if (active) {
+                    var _val = _toKatakana(baseKana + values.join(''));
+                    elKana.val(_val);
+                }
+            }
+        };
+        function _stateClear() {
+            baseKana = '';
+            flagConvert = false;
+            ignoreString = '';
+            input = '';
+            values = [];
+        };
+        function _stateInput() {
+            baseKana = elKana.val();
+            flagConvert = false;
+            ignoreString = elName.val();
+        };
+        function _stateConvert() {
+            baseKana = baseKana + values.join('');
+            flagConvert = true;
+            values = [];
+        };
+        function _toKatakana(src) {
+            if (options.katakana) {
+                var c, i, str;
+                str = new String;
+                for (i = 0; i < src.length; i++) {
+                    c = src.charCodeAt(i);
+                    if (_isHiragana(c)) {
+                        str += String.fromCharCode(c + 96);
+                    } else {
+                        str += src.charAt(i);
+                    }
+                }
+                return str;
+            } else {
+                return src;
+            }
+        }
+    };
+})(jQuery);
